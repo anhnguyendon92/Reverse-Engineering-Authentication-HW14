c0ad91a4d29ba349f03737e1576d14ef6b4da8fc
Anh Nguyen <anhnguyendon92@gmail.com>
2020-10-15 23:15:14 -0700

    Rewriting code

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+var passport = require("./Develop/config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
-var db = require("./models");
+var db = require("./Develop/models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
@@ -19,8 +19,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
+require("./Develop/routes/html.routes")(app);
+require("./Develop/routes/api-routes")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
